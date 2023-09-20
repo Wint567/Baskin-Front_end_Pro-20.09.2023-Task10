7bfa0f150fff4bbd851c51372d544e9358e3fc61
Wint567 <134635875+Wint567@users.noreply.github.com>
2023-09-20 13:23:49 +0300

    Task10

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = parseFloat(prompt("Введіть перше число"));
+var b = parseFloat(prompt("Введіть друге число"));
+var c = parseFloat(prompt("Введіть третє число"));
+
+var average = (a + b + c) / 3;
+
+alert("Середнє арифметичне трьох чисел " + average);
+
